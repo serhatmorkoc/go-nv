1b60a000c97d73ebd5712ea054af8da112cfa274
serhat morkoç <serhatmorkoc@gmail.com>
2019-11-07 12:11:00 +0300

    --comm

--- nv.go
+++ nv.go
@@ -7,3 +7,26 @@ type Response struct {
 	Data         []byte
 }
 
+func crc16(data []byte) []byte {
+	seed := uint16(0xFFFF)
+	poly := uint16(0x8005)
+	crc := seed
+
+	for _, d := range data {
+		crc ^= uint16(d) << 8
+		for i := 0; i < 8; i++ {
+			bit := (crc & 0x8000) != 0
+			crc <<= 1
+			if bit {
+				crc ^= poly
+			}
+		}
+	}
+
+	b := [2]byte{
+		byte(crc & 0xFF),
+		byte((crc >> 8) & 0xFF),
+	}
+
+	return b[:]
+}
