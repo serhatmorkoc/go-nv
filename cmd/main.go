bc466069e4b22df8fbc5f86a20bd9c5d3baa0b59
serhat morkoç <serhatmorkoc@gmail.com>
2019-11-08 11:58:33 +0300

    --comm

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"github.com/serhatmorkoc/go-nv"
+	"time"
+)
+
+func main() {
+
+	nv := nv.NewService(&nv.Config{
+		PortName:    "COM3",
+		Address:     0,
+		ReadTimeout: time.Millisecond * 50,
+		BaudRate:    9600,
+	})
+
+	err := nv.Connect()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nv.Sync()
+	nv.Enable()
+
+
+
+
+}
